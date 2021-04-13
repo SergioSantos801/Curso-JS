6a6814f164f2e413e83af8f01ff6e3a7289031f6
Sérgio <64284595+SergioSantos801@users.noreply.github.com>
2021-04-13 18:21:05 -0300

    primeiros script

--- aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 70.5
-console.log(`A velocidade do seu carro é ${vel} km/h`)
-if(vel > 60) {// condição simples
-    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
--- aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var pais = 'Brasil'
-console.log(`vivendo em ${pais}`)
-if (pais == `Brasil`) {
-    console.log(`Voê é brasileiro`)
-}else{
-    console.log(`Você é estrangeiro!`)
-}
