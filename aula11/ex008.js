d38edc7a20b663ffedbf76d6bca4666cff130c92
Sérgio <64284595+SergioSantos801@users.noreply.github.com>
2021-06-24 11:42:14 -0300

    aula 10

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel> 50) { // condição simples
+    console.log(`Você ultrapoçou a velocidade permitida. MULTADO !`)
+} 
+console.log('Dirija sempre usando o sinto de segurança')
